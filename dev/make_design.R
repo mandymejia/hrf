2b7ad8220dddafb01b8893f186d266452cb3c76e
7konigsberg <zeshawn.zahid@gmail.com>
2025-08-28 23:12:09 -0400

    Added make_design test in dev for computational speed debugging

--- dev/make_design.R
+++ dev/make_design.R
@@ -1,4 +1,9 @@
 library(here)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(tictoc)
+
 
 ciftiTools::ciftiTools.setOption('wb_path','/Applications/workbench/bin_macosxub/wb_command')
 
@@ -8,9 +13,24 @@ devtools::load_all("~/Documents/Github/hrf-z") # hrf-z is Zeshawn's branch with 
 session_data <- readRDS(here("dev", "fixtures", "session_data_motorlr_4s.rds"))
 EVs <- session_data[["EVs_list"]][[1]]
 
-design_3 <- make_design(
-  EVs = EVs, nTime = 284, TR = 0.72, dHRF = 0,
+tic("***make_design when h")
+design_h <- make_design(
+  EVs = EVs, nTime = 284, TR = 0.72, dHRF = 1,
   onset = TRUE, offset = TRUE,
   a1 = 3, b1 = 1/2, c = 1/6,
-  a2 = 8, b2 = 1/2
+  a2 = 8, b2 = 1/2, displace_h = FALSE
 )
+toc()
+
+tic("***make_design when h[-1]")
+design_hneg <- make_design(
+  EVs = EVs, nTime = 284, TR = 0.72, dHRF = 1,
+  onset = TRUE, offset = TRUE,
+  a1 = 3, b1 = 1/2, c = 1/6,
+  a2 = 8, b2 = 1/2, displace_h = TRUE
+)
+toc()
+
+
+plot(design_hneg)
+plot(design_h)
